5a99f3dd0395cb9d891a1bdd5734c84e133c09ec
saurav-commits <sauravsky55@yahoo.com>
2026-01-04 21:26:31 +0530

    Day25

---
+++ daily coding practice/merge2sortedarr.js
@@ -0,0 +1,103 @@
+// You are given two sorted arrays (non-decreasing order).
+// Your task is to merge them into one sorted array.
+
+// arr1 = [1, 3, 5]
+// arr2 = [2, 3, 6]
+// Output = [1, 2, 3, 3, 5, 6]
+
+// Key observation (THIS is the pattern)
+
+// Both arrays are already sorted
+
+// So you don’t need to sort again
+
+// You just need to compare elements step by step
+
+// 👉 This immediately suggests the two-pointer approach.
+
+// Complexity (always say this)
+
+// Time: O(n + m)
+
+// Space: O(n + m) (new merged array)
+
+// Optimal Approach (Two Pointers)
+// Step-by-step logic (interview explanation)
+
+// 1️⃣ Take two pointers:
+
+// i → start of first array
+
+// j → start of second array
+
+// 2️⃣ Compare:
+
+// smaller element goes into result
+
+// move that pointer forward
+
+// 3️⃣ When one array ends:
+
+// copy the remaining elements of the other array
+
+
+// function mergeSortedArrays(arr1, arr2){
+//     let i=0, j=0;
+//     let result = [];
+
+//     while(i<arr1.length && j <arr2.length) {
+//         if(arr1[i] <= arr2[j]) {
+//             result.push(arr1[i]);
+//             i++;
+//         } else {
+//             result.push(arr2[j]);
+//             j++;
+//         }
+//     }
+
+//     while(i<arr1.length) {
+//         result.push(arr1[i]);
+//         i++;
+//     }
+
+//     while(j<arr2.length){
+//         result.push(arr2[j]);
+//         j++;
+//     }
+
+//     return result;
+// }
+
+
+// console.log(mergeSortedArrays([1,3,5],[2,3,6]));
+
+
+function mergeInPlace(arr1, n, arr2, m) {
+    let i=n-1;
+    let j=m-1;
+    let k=n+m-1;
+
+    while(i>=0 && j>=0){
+        if(arr1[i] > arr2[j]) {
+            arr1[k] = arr1[i];
+            i--;
+        } else {
+            arr1[k]=arr2[j];
+            j--;
+        }
+        k--;
+    }
+
+    while(j>=0){
+        arr1[k]=arr2[j];
+        j--;
+        k--;
+    }
+
+    return arr1;
+}
+
+let arr1 = [2,4,6];
+let arr2 = [1,2,3,5,0,0,0];
+
+console.log(mergeInPlace(arr1, 3, arr2, 3));
