d16aa2ac60e1c4bf932ed974145cc245ceecf560
saurav-commits <sauravsky55@yahoo.com>
2026-04-05 23:56:38 +0530

    Day65

---
+++ daily coding practice/judgecircles.js
@@ -0,0 +1,15 @@
+function judgeCircle(moves) {
+    let x = 0, y = 0;
+
+    for (let move of moves) {
+        if (move === 'U') y++;
+        else if (move === 'D') y--;
+        else if (move === 'R') x++;
+        else if (move === 'L') x--;
+    }
+
+    return x === 0 && y === 0;
+}
+
+const moves = "UD";
+console.log(judgeCircle(moves));
