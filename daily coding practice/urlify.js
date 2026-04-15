525d8ba90e429f1a28131cff92e272b603b4a961
saurav-commits <sauravsky55@yahoo.com>
2026-04-15 23:30:09 +0530

    day 67

---
+++ daily coding practice/urlify.js
@@ -0,0 +1,13 @@
+function urlify(str){
+    let result = "";
+    for(let i=0; i<str.length; i++){
+        if(str[i] === " "){
+            result += "%20";
+        } else {
+            result += str[i];
+        }
+    }
+    return result;
+}
+
+console.log(urlify("Mr John Smith"));
