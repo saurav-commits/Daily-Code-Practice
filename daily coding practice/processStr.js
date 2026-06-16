0da0fde9a742beac6b6ba249581cfb2e002e2ba6
Saurav Kumar Yadav <77330046+saurav-commits@users.noreply.github.com>
2026-06-16 23:38:51 +0530

    Create processStr.js
    Day 85

---
+++ daily coding practice/processStr.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+function processString(s) {
+    let result = "";
+
+    for (const ch of s) {
+        if (ch >= 'a' && ch <= 'z') {
+            result += ch;
+        } else if (ch === '*') {
+            if (result.length > 0) {
+                result = result.slice(0, -1);
+            }
+        } else if (ch === '#') {
+            result += result;
+        } else if (ch === '%') {
+            result = result.split("").reverse().join("");
+        }
+    }
+
+    return result;
+}
